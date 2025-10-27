1c916a8986255573f9afb0b9d7a6728436fa11ea
NarimanAhmed4002 <narimanahmed0987@gmail.com>
2025-10-28 00:00:03 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { bootstrap } from "./app.controller.js";
+import dotenv from "dotenv";
+dotenv.config({path:"./"});
 const app = express();
 const port = process.env.PORT;
 bootstrap(app, express);
